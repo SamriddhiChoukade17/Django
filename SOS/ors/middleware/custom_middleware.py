d0ac2e691341465c7bb8321dfcadfc7c8b83ed3a
SamriddhiChoukade17 <samriddhichoukade.tech@gmail.com>
2026-03-24 13:50:36 +0530

    done

---
+++ SOS/ors/middleware/custom_middleware.py
@@ -0,0 +1,33 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+class SimpleMiddleware:
+    count = 0
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        SimpleMiddleware.count += 1
+        print("Middleware: ", SimpleMiddleware.count)
+        res = self.get_response(request)
+        # return HttpResponse("<center><h1>Welcome to Middleware</h1></center>")
+        return res
+
+
+class FrontCtlMiddleware:
+
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        if request.path_info in ['/', '/ors/signin/', '/ors/signup/', '/ors/welcome/', '/ors/logout/']:
+            return self.get_response(request)
+
+        if request.session["firstName"] == None:
+            message = "Session Expired...Please Login Again!!"
+
+            return render(request, 'login2.html', {'message': message})
+
+        return self.get_response(request)
